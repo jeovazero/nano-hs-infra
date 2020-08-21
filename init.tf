17d7e5f40055029c2eb8d072dfbb4bd89e96cfdd
jeovazero <jeovapereira2000@live.com>
2020-08-21 00:02:14 -0300

    Add internet gateway and route table

--- init.tf
+++ init.tf
@@ -37,6 +37,121 @@ resource "aws_subnet" "nano-subnet" {
   }
 }
 
+# Security Group operates at the instance level
+resource "aws_security_group" "nano-sec" {
+  name   = "nano-sec-main"
+  vpc_id = aws_vpc.nano-vpc.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow TLS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Nano Sec Group"
+  }
+}
+
+# Network ACL operates at the subnet level
+resource "aws_network_acl" "nano-acl" {
+  vpc_id     = aws_vpc.nano-vpc.id
+  subnet_ids = [aws_subnet.nano-subnet.id]
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 300
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 310
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name = "Nano Net ACL"
+  }
+}
+
+resource "aws_internet_gateway" "nano-gw" {
+  vpc_id = aws_vpc.nano-vpc.id
+  tags = {
+    Name = "Nano Internet Gateway"
+  }
+}
+
+# The Route Table determines where network traffic from your subnet
+# or gateway is directed.
+resource "aws_route_table" "nano-rt" {
+  vpc_id = aws_vpc.nano-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nano-gw.id
+  }
+
+  tags = {
+    Name = "Nano Route Table"
+  }
+}
+
+# The association between a route table and a subnet
+resource "aws_route_table_association" "nano-assoc" {
+  subnet_id      = aws_subnet.nano-subnet.id
+  route_table_id = aws_route_table.nano-rt.id
+}
+
 data "aws_ami" "amzn" {
   owners = ["amazon"]
   filter {
@@ -55,13 +170,17 @@ resource "aws_key_pair" "jeogod-key" {
 }
 
 resource "aws_instance" "nano_hs" {
-  ami           = data.aws_ami.amzn.id
-  instance_type = "t3a.nano"
-  key_name      = aws_key_pair.jeogod-key.key_name
+  ami                    = data.aws_ami.amzn.id
+  vpc_security_group_ids = [aws_security_group.nano-sec.id]
+  subnet_id              = aws_subnet.nano-subnet.id
+  private_ip             = "10.0.1.144"
+  instance_type          = "t3.micro"
+  associate_public_ip_address = true
+  key_name               = aws_key_pair.jeogod-key.key_name
   tags = {
     Name = "nano-hs"
   }
-  depends_on = [aws_key_pair.jeogod-key]
+  depends_on = [aws_key_pair.jeogod-key, aws_route_table_association.nano-assoc]
 }
 
 /*
